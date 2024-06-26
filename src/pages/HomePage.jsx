1cebe46c4bebba267f2dbea5d66d95a3b31f8a2b
Isaac Vazquez <ivzc07@gmail.com>
2024-06-26 01:17:56 -0600

    add hover homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,8 +6,8 @@ export default function HomePage() {
             <h1 className="text 4-xl font-bold text-center text-6xl" >Fake Store </h1>
             <p className="text-center text-4xl">Keeping It Real 🫀 </p>
             <div className='flex flex-row gap-8 text-center text-3xl'>
-                <Link to="/login">{" { login } "}</Link>
-                <Link to="/products">{" { products } "}</Link>
+                <Link to="/login" className='hover:text-white/50'>{" { login } "}</Link>
+                <Link to="/products" className='hover:text-white/50'>{" { products } "}</Link>
                 
             </div>
         </div>
